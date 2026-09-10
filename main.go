3aa579f5ae10db6db36f2771fe8287a87d27b350
rantoinne <raviast1719@outlook.com>
2026-09-11 00:42:34 +0530

    feat(logging): logs setup

--- main.go
+++ main.go
@@ -5,9 +5,21 @@ import (
 	"os"
 
 	ncmindex "github.com/rantoinne/ncm/cmd/ncm-index"
+	"github.com/rantoinne/ncm/internal/logging"
 )
 
 func main() {
+	logfile, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error opening log file: %v\n", err)
+		os.Exit(1)
+	}
+	defer logfile.Close()
+
+	os.Stdout = logfile
+	os.Stderr = logfile
+
+	logging.Setup(logging.Options{})
 	args := os.Args[1:]
 	if err := ncmindex.Run(args); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
